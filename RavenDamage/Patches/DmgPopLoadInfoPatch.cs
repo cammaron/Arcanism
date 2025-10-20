766cdac0d377201f129b782d8779e3f9ebf399d2
Cameron Duff <dragonfistlimitless@gmail.com>
2025-10-20 11:14:10 +1100

    Renaming RavenDamage to BigDamage part 1

--- RavenDamage/Patches/DmgPopLoadInfoPatch.cs
+++ RavenDamage/Patches/DmgPopLoadInfoPatch.cs
@@ -2,7 +2,7 @@
 using BepInEx;
 using HarmonyLib;
 
-namespace RavenDamage
+namespace BigDamage.Patches
 {
     [HarmonyPatch(typeof(DmgPop), nameof(DmgPop.LoadInfo), new[] { typeof(int), typeof(bool), typeof(GameData.DamageType), typeof(Transform) })]
     public class DmgPopLoadInfoPatch
@@ -16,34 +16,32 @@ namespace RavenDamage
             {
 				initialised = true;
 				initialScale = __instance.transform.localScale; // Assuming they all have the same standard scale...
-				Main.Log.LogInfo("Initialising DmgPop LoadInfo patch. Initial scale of DmgPop instance is: " + initialScale);
             }
 
 			__instance.Num.enableVertexGradient = false;
-			//__instance.speed = 5f;
 			Traverse.Create(__instance).Field("sideDir").SetValue(new Vector3(Random.Range(-1f, 1f), 0, 0));
 
 			float minDistance = 3.0f;
 			float distanceFromCamera = Vector3.Distance(__instance.transform.position, GameData.CamControl.transform.position);
-			float distanceScaleMultiplier = Mathf.Max(1, ((distanceFromCamera - minDistance) / 17f) * 2f);
-			__instance.transform.localScale = initialScale * 2.5f * distanceScaleMultiplier;
+			float distanceScaleMultiplier = Mathf.Max(1, ((distanceFromCamera - minDistance) / Main.distanceToDoubleSize.Value) * 2f);
+			__instance.transform.localScale = initialScale * Main.baseSizeFactor.Value * distanceScaleMultiplier;
 
 			switch (_type)
 			{
 				case GameData.DamageType.Physical:
-					__instance.Num.color = new Color32(140, 140, 140, 255); // Medium grey
+					__instance.Num.color = Main.physColour;
 					break;
 				case GameData.DamageType.Magic:
-					__instance.Num.color = new Color32(100, 190, 255, 255); // Sky blue
+					__instance.Num.color = Main.magColour;
 					return;
 				case GameData.DamageType.Elemental:
-					__instance.Num.color = new Color32(255, 130, 40, 255); // Orange
+					__instance.Num.color = Main.eleColour;
 					return;
 				case GameData.DamageType.Void:
-					__instance.Num.color = new Color32(200, 80, 200, 255); // Purple
+					__instance.Num.color = Main.voidColour;
 					return;
 				case GameData.DamageType.Poison:
-					__instance.Num.color = new Color32(140, 255, 80, 255); // Sorta acid green
+					__instance.Num.color = Main.poisonColour;
 					return;
 				default:
 					return;
