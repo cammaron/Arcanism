d583ddfa6ba1075e47024d25019f9230b2d2ca77
Cameron Duff <dragonfistlimitless@gmail.com>
2025-10-27 17:34:24 +1100

    Add Item.CalcDmg patch so that blue/purple blessing scale weapon damage properly

---
+++ Patches/Item.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Arcanism.Patches
+{
+	/* Blessings need to be updated to give more than just +1 damage to a weapon, because that's completely useless */
+	[HarmonyPatch(typeof(Item), nameof(Item.CalcDmg))]
+	public class Item_CalcDmg
+	{
+
+		static bool Prefix(Item __instance, ref int __result, int _stat, int _qual)
+		{
+			if (_qual == 2) __result = Mathf.RoundToInt(_stat * 1.25f);
+			else if (_qual == 3) __result = Mathf.RoundToInt(_stat * 1.5f);
+			__result = _stat;
+			return false;
+		}
+	}
+}
